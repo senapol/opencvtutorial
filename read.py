a80bf3e4207a7d0d49bdc42a927670dc1b38503c
senapol <127954337+senapol@users.noreply.github.com>
2024-06-08 00:34:11 +0100

    1

--- read.py
+++ read.py
@@ -1,7 +1,17 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat.jpg')  #returns image as matrix of pixels
+# img = cv.imread('Resources/Photos/cat.jpg')  #returns image as matrix of pixels
 
-cv.imshow('Cat', img)  #displays image
+# cv.imshow('Cat', img)  #displays image
+
+# Reading videos
+capture = cv.VideoCapture('Resources/Videos/dog.mp4') #0 is the default camera
+
+while True:
+    isTrue, frame = capture.read() # reads video frame by frame
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'): #if 'd' key is pressed, break
+        break
 
 cv.waitKey(0) #waits for key press to close window
