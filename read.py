aefdee3cff1efb58d616951a4172dfca017598ad
senapol <127954337+senapol@users.noreply.github.com>
2024-06-09 00:59:13 +0100

    2

--- read.py
+++ read.py
@@ -4,6 +4,9 @@ import cv2 as cv
 
 # cv.imshow('Cat', img)  #displays image
 
+
+# cv.waitKey(0) #waits for key press to close window
+
 # Reading videos
 capture = cv.VideoCapture('Resources/Videos/dog.mp4') #0 is the default camera
 
@@ -14,4 +17,6 @@ while True:
     if cv.waitKey(20) & 0xFF==ord('d'): #if 'd' key is pressed, break
         break
 
-cv.waitKey(0) #waits for key press to close window
+capture.release() #release video
+cv.destroyAllWindows() #destroy windows
+# -215 Assertion failed error means that the video ran out of frames
---
+++ rescale.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+# img = cv.imread('Resources/Photos/cat.jpg')  #returns image as matrix of pixels
+
+# cv.imshow('Cat', img)  #displays image
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+# Reading videos
+capture = cv.VideoCapture('Resources/Videos/dog.mp4') #0 is the default camera
+
+while True:
+    isTrue, frame = capture.read() # reads video frame by frame
+
+    frame_resized = rescaleFrame(frame)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'): #if 'd' key is pressed, break
+        break
+
+capture.release() #release video
+cv.destroyAllWindows() #destroy windows
+# -215 Assertion failed error means that the video ran out of frames
