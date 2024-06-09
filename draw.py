264c6a5bfdceb45b6858c5c6f7e032876a73d1bc
senapol <127954337+senapol@users.noreply.github.com>
2024-06-09 23:28:04 +0100

    !

--- draw.py
+++ draw.py
@@ -4,7 +4,22 @@ import numpy as np
 blank = np.zeros((500,500,3), dtype='uint8')  #creates a black image
 cv.imshow('Blank', blank)
 
-img = cv.imread('Resources/Photos/cat.jpg')  #returns image as matrix of pixels
-cv.imshow('Cat', img)  #displays image
+# 1. Paint the image a certain color
+blank[200:300,300:400] = 0,0,255
+cv.imshow('Green', blank)
+
+# 2. Draw a rectangle
+cv.rectangle(blank, (0,0), (250,300), (0,255,0), thickness=2)
+# or cv.filled/-1 for filled
+
+cv.imshow('Rectangle', blank)
+
+# 3. Draw a circle
+cv.circle(blank, (250,250), 40, (250,0,0), thickness=-1)
+cv.imshow('Circle', blank)
+
+# 4. Draw a line
+cv.line(blank, (50,50), (200,200), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
 
 cv.waitKey(0)
